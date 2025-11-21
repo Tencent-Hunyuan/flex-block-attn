8873e82bb3191f1efb5a56b794f0d3dea65cae90
yuanbopeng <yuanbopeng@tencent.com>
2025-11-21 19:18:34 +0800

    Update __init__.py

--- flex_block_attn/__init__.py
+++ flex_block_attn/__init__.py
@@ -4,9 +4,9 @@ from flex_block_attn.flex_block_attn_interface import (
     flex_block_attn_func,
 )
 
-from flex_block_attn.ssta_attention import (
-    create_sta_3d_mask,
-    create_ssta_3d_mask,
-    create_moba_3d_mask,
-    ssta_3d_attention
-)
+#from flex_block_attn.ssta_attention import (
+#    create_sta_3d_mask,
+#    create_ssta_3d_mask,
+#    create_moba_3d_mask,
+#    ssta_3d_attention
+#)
